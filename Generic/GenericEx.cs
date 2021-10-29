c125e5a922aa313c305c14da8210c84721334116
santoshinawkhare <gunjannawkhare1@gmail.com>
2021-10-29 16:58:55 +0530

    generic is added

---
+++ Generic/GenericEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    class GenericEx
+    {
+        public void intMaximum(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("Maximum No is :" + first);
+            }
+            if (second.CompareTo(third) > 0 && second.CompareTo(first) > 0)
+            {
+                Console.WriteLine("Maximum No is :" + second);
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("Maximum No is :" + third);
+            }
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            GenericEx find = new GenericEx();
+            find.intMaximum(10, 20, 30);
+        }
+    }
+}
